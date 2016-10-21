3192d3864c0346b3f9e84e02c28d16cf3832c012
Peter Krieg <pkrieg2@gmail.com>
2016-10-20 21:50:34 -0400

    used simple express app to serve static files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const compression = require('compression');
+
+const app = express();
+
+// port for slys.  is defined on port 3010 in .env file on server
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Listening to port: ${port}.`);
+});
+
+// use gzip for compression, faster page loading
+app.use(compression());
+
+// Provide path where to serve index.html file
+app.use(express.static(`${__dirname}`));
+
